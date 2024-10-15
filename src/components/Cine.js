594e033ed1528ab1299f7b4d8136c70328738d70
serraguti <pacoserranox@gmail.com>
2024-10-15 12:39:21 +0200

    aaa

---
+++ src/components/Cine.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Cine extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Cine Component</h1>
+        <img src="https://pbs.twimg.com/media/FiMd8cTWAAwdoWu.jpg:large"
+        style={{width: "150px", height: "150px"}}/>
+    </div>
+    )
+  }
+}
---
+++ src/components/Musica.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Musica extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Musica</h1>
+        <img src="https://revistafuneraria.com/wp-content/uploads/2021/04/pelicula_coco-1.jpg"
+        style={{width: "150px", height: "150px"}}/>
+      </div>
+   )
+  }
+}
